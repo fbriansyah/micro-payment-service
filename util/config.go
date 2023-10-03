07a12ae86b44445c8bf70814ea83b5a09c692802
Febriansyah <fbriansyah@gmail.com>
2023-10-03 11:24:18 +0700

    add biller endpoint config

--- util/config.go
+++ util/config.go
@@ -7,11 +7,12 @@ import (
 // Config stores all configuration of the application.
 // The values are read by viper from a config file or environment variable.
 type Config struct {
-	Environment  string `mapstructure:"ENVIRONMENT"`
-	DBDriver     string `mapstructure:"DB_DRIVER"`
-	DBSource     string `mapstructure:"DB_SOURCE"`
-	MigrationURL string `mapstructure:"MIGRATION_URL"`
-	GrpcPort     int    `mapstructure:"GRPC_PORT"`
+	Environment    string `mapstructure:"ENVIRONMENT"`
+	DBDriver       string `mapstructure:"DB_DRIVER"`
+	DBSource       string `mapstructure:"DB_SOURCE"`
+	MigrationURL   string `mapstructure:"MIGRATION_URL"`
+	GrpcPort       int    `mapstructure:"GRPC_PORT"`
+	BillerEndpoint string `mapstructure:"BILLER_ENDPOINT"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
