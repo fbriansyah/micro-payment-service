72b04c24b7d7c3d5e8c1a908603af096dac73195
Febriansyah <fbriansyah@gmail.com>
2023-10-03 10:05:02 +0700

    add database port

---
+++ internal/port/database_port.go
@@ -0,0 +1,20 @@
+package port
+
+import (
+	"context"
+
+	"github.com/fbriansyah/micro-payment-service/internal/adapter/postgresdb"
+	"github.com/google/uuid"
+)
+
+type DatabasePort interface {
+	CreateOutlet(ctx context.Context, arg postgresdb.CreateOutletParams) (postgresdb.Outlet, error)
+	CreateProduct(ctx context.Context, arg postgresdb.CreateProductParams) (postgresdb.Product, error)
+	CreateRequestLog(ctx context.Context, arg postgresdb.CreateRequestLogParams) (postgresdb.RequestLog, error)
+	CreateTransaction(ctx context.Context, arg postgresdb.CreateTransactionParams) (postgresdb.Transaction, error)
+	GetOutletByUserID(ctx context.Context, user uuid.UUID) (postgresdb.Outlet, error)
+	GetProductByCode(ctx context.Context, productCode string) (postgresdb.Product, error)
+	GetRequestLogByID(ctx context.Context, id uuid.UUID) (postgresdb.RequestLog, error)
+	GetTransactionByID(ctx context.Context, id uuid.UUID) (postgresdb.Transaction, error)
+	UpdateOutletDeposit(ctx context.Context, arg postgresdb.UpdateOutletDepositParams) (postgresdb.Outlet, error)
+}
