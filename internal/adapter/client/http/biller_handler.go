da1fad6ad0565c7d46f3dd9304971d74aa2b1e4d
Febriansyah <fbriansyah@gmail.com>
2023-10-03 13:23:13 +0700

    add payment and test

--- internal/adapter/client/http/biller_handler.go
+++ internal/adapter/client/http/biller_handler.go
@@ -11,12 +11,6 @@ import (
 	dmbiller "github.com/fbriansyah/micro-payment-service/internal/application/domain/biller"
 )
 
-type PaymentResponse struct {
-	Billing          dmbiller.Bill `json:"bill"`
-	RefferenceNumber string        `json:"refference_number"`
-	CreatedAt        time.Time     `json:"created_at"`
-}
-
 type InquiryResponse struct {
 	Error   bool          `json:"error"`
 	Message string        `json:"message"`
@@ -55,12 +49,65 @@ func (a *HttpAdapter) Inquiry(billNumber string) (InquiryResponse, error) {
 		return InquiryResponse{}, err
 	}
 
-	if response.StatusCode != http.StatusOK {
+	if (response.StatusCode != http.StatusOK) || responseJson.Error {
 		return InquiryResponse{}, errors.New(responseJson.Message)
 	}
 
-	if responseJson.Error {
-		return InquiryResponse{}, errors.New(responseJson.Message)
+	return responseJson, nil
+}
+
+type PaymentData struct {
+	Billing          dmbiller.Bill `json:"bill"`
+	RefferenceNumber string        `json:"refference_number"`
+	CreatedAt        time.Time     `json:"created_at"`
+}
+
+type PaymentResponse struct {
+	Error   bool        `json:"error"`
+	Message string      `json:"message"`
+	Data    PaymentData `json:"data,omitempty"`
+}
+
+type PaymentRequest struct {
+	BillNumber       string `json:"bill_number"`
+	RefferenceNumber string `json:"refference_number"`
+	TotalAmount      int64  `json:"total_amount"`
+}
+
+// Payment send request to biller endpoint
+func (a *HttpAdapter) Payment(req PaymentRequest) (PaymentResponse, error) {
+
+	requestJson, err := json.Marshal(req)
+	if err != nil {
+		return PaymentResponse{}, errors.New("error parsing payment request")
+	}
+
+	request, err := http.NewRequest(
+		"POST",
+		a.endpoint+"/payment",
+		bytes.NewBuffer(requestJson),
+	)
+	if err != nil {
+		fmt.Println(err)
+		return PaymentResponse{}, errors.New("error create request")
+	}
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := a.client.Do(request)
+	if err != nil {
+		return PaymentResponse{}, err
+	}
+
+	var responseJson PaymentResponse
+
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&responseJson)
+	if err != nil {
+		return PaymentResponse{}, errors.New("error decode response")
+	}
+
+	if (response.StatusCode != http.StatusOK) || responseJson.Error {
+		return PaymentResponse{}, errors.New(responseJson.Message)
 	}
 
 	return responseJson, nil
--- internal/adapter/client/http/biller_test.go
+++ internal/adapter/client/http/biller_test.go
@@ -3,6 +3,7 @@ package httpclient
 import (
 	"testing"
 
+	"github.com/fbriansyah/micro-payment-service/util"
 	"github.com/stretchr/testify/require"
 )
 
@@ -28,4 +29,19 @@ func TestInquiryNotFound(t *testing.T) {
 	response, err := testAdapter.Inquiry("6310233333330")
 	require.Error(t, ErrorBillNotFound, err)
 	require.Empty(t, response)
+}
+
+func TestPayment(t *testing.T) {
+	billNumber := "6310233333334"
+	inqResponse, err := testAdapter.Inquiry(billNumber)
+	require.NoError(t, err)
+	require.NotEmpty(t, inqResponse)
+
+	payResponse, err := testAdapter.Payment(PaymentRequest{
+		BillNumber:       billNumber,
+		RefferenceNumber: util.RandomRefferenceNumber(),
+		TotalAmount:      inqResponse.Data.TotalAmount,
+	})
+	require.NoError(t, err)
+	require.NotEmpty(t, payResponse)
 }
