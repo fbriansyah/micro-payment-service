c21d084d0c0544414e0672ee6433d17182d638f0
Febriansyah <fbriansyah@gmail.com>
2023-10-04 08:51:44 +0700

    fix typo

--- internal/port/service_port.go
+++ internal/port/service_port.go
@@ -7,7 +7,7 @@ import (
 	dmrequest "github.com/fbriansyah/micro-payment-service/internal/application/domain/request"
 )
 
-type PaymetServer interface {
+type PaymetService interface {
 	Inquiry(ctx context.Context, arg dmrequest.InquryRequestParams) (dmlog.RequestLog, error)
 	Payment(ctx context.Context, arg dmrequest.PaymentRequestParams)
 }
