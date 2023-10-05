8af78c15febb3d7506f8c4c91f866be273b4b9b3
Febriansyah <fbriansyah@gmail.com>
2023-10-05 19:27:28 +0700

    add dummy data

---
+++ db/migration/000003_init-data.up.sql
@@ -0,0 +1,5 @@
+INSERT INTO products (product_code, product_name, product_endpoint)
+VALUES ('P01', 'Biller 1', 'http://billerms:8080');
+
+INSERT INTO outlets (id, "user", deposit, is_active)
+VALUES ('bb1baa44-05c2-45ae-851c-e1dd6dfa005c', 'ad61a78b-0f66-44d6-a579-695d8fec883f', 1000000, true);
